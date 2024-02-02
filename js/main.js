5a080984010a49ab6012480da49aaac7651927dd
mauritsf <57948944+mauritsf15@users.noreply.github.com>
2024-02-02 18:02:50 +0100

    pc update, no local storage

--- js/main.js
+++ js/main.js
@@ -1,44 +1,48 @@
-const hoursDone = document.querySelector('.hours-done');
-const hoursToGo = document.querySelector('.hours-to-go');
-const days = document.querySelector('.days');
-const weeks = document.querySelector('.weeks');
-const btn = document.querySelector('.btn');
+const hoursDone = document.querySelector('.hours-completed');
+const hoursToGo = document.querySelector('.hours-left');
+const days = document.querySelector('.days-left');
+const weeks = document.querySelector('.weeks-left');
+const btn = document.querySelector('.button');
 const input = document.querySelector('.input');
 const value = document.querySelector('.progress-bar');
 const latestDate = document.querySelector('.date');
 
-const hoursToGet = 1000;
-let currentHours = parseFloat(localStorage.getItem('internshipHours'));
-
-if (isNaN(currentHours)) {
-    currentHours = 0;
+function qs(className) {
+    return document.querySelector(`#${className}`).value;
 }
 
-function update() {
-    let latestUpdate = localStorage.getItem('latestdate');
+function update(hoursPerDay, daysPerWeek, hoursToGet, currentHours) {
+    // let latestUpdate = localStorage.getItem('latestdate');
     hoursDone.innerText = currentHours;
-    latestDate.innerText = latestUpdate;
+    // latestDate.innerText = latestUpdate;
     hoursToGo.innerText = hoursToGet - currentHours;
-    days.innerText = (hoursToGet - currentHours) / 8;
-    weeks.innerText = (hoursToGet - currentHours) / 40;
+    days.innerText = Math.round(((hoursToGet - currentHours) / hoursPerDay)*100) / 100;
+    weeks.innerText = Math.round(((hoursToGet - currentHours) / (hoursPerDay * daysPerWeek)) *100) / 100;
     let num = currentHours / hoursToGet;
     value.style.width = `${Math.round(num * 10000) / 100}%`
     value.innerText = `${Math.round(num * 10000) / 100}%`
 }
 
-update();
+// update();
 
-function addHours(hours) {
-    if (hours > 0) {
-        input.value = '';
-        currentHours += parseFloat(hours);
-        localStorage.setItem('internshipHours', currentHours);
-        const date = new Date()
-        localStorage.setItem('latestdate', date.getDate() + '/' + (date.getMonth() + 1));
-    } else {
-        alert('Dat werkt niet zo! Probeer het opnieuw met een getal.')
-    }
-    update();
-}
+// function addHours(hours) {
+//     if (hours > 0) {
+//         input.value = '';
+//         currentHours += parseFloat(hours);
+//         localStorage.setItem('internshipHours', currentHours);
+//         const date = new Date()
+//         localStorage.setItem('latestdate', date.getDate() + '/' + (date.getMonth() + 1));
+//     } else {
+//         alert('Dat werkt niet zo! Probeer het opnieuw met een getal.')
+//     }
+//     update();
+// }
 
-btn.addEventListener('click', function() {addHours(input.value)})
+btn.addEventListener('click', function() {
+    update(
+        qs('hours-per-day'),
+        qs('days-per-week'),
+        qs('hours-to-go'),
+        qs('hours-done')
+    )
+})
